a46bab57a6459a8b747eddec067148d8d6a90482
AhtishamKingdomVision <110660474+AhtishamKingdomVision@users.noreply.github.com>
2026-07-22 13:02:31 +0500

    deleted files

--- wp-content/themes/22-7-2026-kingdomvision-jsnew/section/inc-textblock.php
+++
@@ -1,9 +0,0 @@
-<?php
-$content = $section['content'];
-echo '<section ' . SectionAttributes($section, 'full-section textblock') . ' ' . BackgroundFromSection($section) . '>';
-	echo '<div class="container">';
-		echo TitleFromSection($section);
-		echo $content;
-	echo '</div>';
-echo '</section>';
-?>
